0c437dc96024bf47bbbe37385fdbe9b9cffacf6e
mun-kyeong <horse6953@naver.com>
2025-09-02 14:46:38 +0900

    Updates

--- bundle.js
+++ bundle.js
@@ -3848,7 +3848,7 @@ var gifAPIService_awaiter = (undefined && undefined.__awaiter) || function (this
     });
 };
 
-const API_KEY = "MISSING_ENV_VAR".GIPHY_API_KEY;
+const API_KEY = "TEX3aXgNjfa3KJSfZBtirwStxjoJ5fky";
 if (!API_KEY) {
     throw new Error('GIPHY_API_KEY is not set in environment variables');
 }
